6cf8b42c5bd8aac0c1978e692cf21de4b1c22ebf
DovaZk <silva.joel.08@gmail.com>
2021-02-03 23:21:06 +0000

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
+
 /**
  * main - Function to show the bites of each data type
  * Return: 0
  **/
+
 int main(void)
 {
 
-printf("Size of a char: %d byte(s)\n", sizeof(c));
-printf("Size of an int: %d byte(s)\n", sizeof(i));
-printf("Size of a long int: %d byte(s)\n", sizeof(li));
-printf("Size of a long long int: %d byte(s)\n", sizeof(lli));
-printf("Size of a float: %d byte(s)\n", sizeof(f));
-return (0);
+  char c;
+  int i;
+  long int li;
+  long long int lli;
+  float f;
+
+  printf("Size of a char: %lu byte(s)\n", sizeof(c));
+  printf("Size of an int: %lu byte(s)\n", sizeof(i));
+  printf("Size of a long int: %lu byte(s)\n", sizeof(li));
+  printf("Size of a long long int: %lu byte(s)\n", sizeof(lli));
+  printf("Size of a float: %lu byte(s)\n", sizeof(f));
+  return (0);
 
 }
