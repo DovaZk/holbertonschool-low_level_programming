004250d2f8cde89541728575dd5f7234957a0032
DovaZk <silva.joel.08@gmail.com>
2021-02-04 20:20:51 +0000

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,7 +8,6 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	if (n == 0)
 	{
 		printf("%d is zero\n", n);
@@ -21,4 +20,3 @@ int main(void)
 		printf("%d is negative\n", n);
 	return (0);
 }
-
