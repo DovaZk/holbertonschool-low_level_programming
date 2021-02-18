787f9a5e8644d6c13985db394d34a9bd1e7fc078
DovaZk <silva.joel.08@gmail.com>
2021-02-18 23:49:32 +0000

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strcmp - Entry point
+ * @s1: pointer type char
+ * @s2: pointer type char
+ * Return: Always 0 (Success)
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (((int) *s1) - ((int) *s2));
+}
