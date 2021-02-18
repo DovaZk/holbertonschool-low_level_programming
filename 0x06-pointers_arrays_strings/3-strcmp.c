6405eef34533d8fc705d06a56d9326b62b7a0029
DovaZk <silva.joel.08@gmail.com>
2021-02-18 23:59:05 +0000

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,5 +8,12 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	return (((int) *s1) - ((int) *s2));
+	int operation;
+
+	do {
+		operation = (int) (*s1 - *s2);
+		s1++;
+		s2++;
+	} while (operation == 0 && (*s1 != '\0' && *s2 != '\0'));
+	return (operation);
 }
