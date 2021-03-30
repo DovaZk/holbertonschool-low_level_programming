d5b83ccdb1258f380ef2ef68d1542da598db28ab
DovaZk <silva.joel.08@gmail.com>
2021-03-30 02:22:31 +0000

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * create_file - function that creates a file.
+ * @filename: pointer to file.
+ * @text_content: string.
+ * Return: 1 or -1.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int i;
+
+	if (filename)
+	{
+		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 00600);
+		if (fd < 0)
+			return (-1);
+
+		if (text_content)
+		{
+			for (i = 0; text_content[i] != '\0'; i++)
+			{
+			}
+			write(fd, text_content, i);
+		}
+		close(fd);
+		return (1);
+	}
+	return (-1);
+}
